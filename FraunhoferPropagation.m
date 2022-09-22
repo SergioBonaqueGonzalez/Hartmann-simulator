aba339818cd67157dd00b874e32be71904bd83a1
Sergio Bonaque-Gonzalez <sergio.bonaque.gonzalez@gmail.com>
2022-09-22 11:32:20 +0100

    Update FraunhoferPropagation.m

--- FraunhoferPropagation.m
+++ FraunhoferPropagation.m
@@ -6,8 +6,8 @@ Its an adaptation of the methods described in:
 - David Voelz - Computational Fourier Optics (2011)
 
 Adapted by Sergio Bonaque-Gonzalez. Optical Engineer.
-sergiob@wooptix.com
-July,2019 - Wooptix S.L.
+sergio.bonaque.gonzalez@gmail.com
+July,2019 
 
 This function calculates the propagation in the Fraunhofer region. A uniform sampling is supossed. 
 
